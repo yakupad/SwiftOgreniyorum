1632d42035d74fd5c3c27c942b7510fced8b62bf
yakupad <yakupad@yandex.com>
2017-01-20 22:02:28 +0300

    hello

---
+++ HelloWorld/main.swift
@@ -0,0 +1,34 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by Yakup Ad on 20.01.2017.
+//  Copyright © 2017 Yakup Ad. All rights reserved.
+//
+
+import Foundation
+//Yazdırma
+print("Merhaba, Dünya!")
+
+//Sabit Tanımlama
+let a : Int = 45, b = 67
+let c = 87
+
+//Değişken Tanımlama
+var z : Double = 89
+var y = 23.9, x : Double = 97
+var toplam : Double
+
+//"let" sözcüğü ile sabitler, "var" sözcüğü ile de değişkenler tanımlanır.
+//Sabitlerin program içerisinde değeri değiştirilemez, değişkenlerin ise program içerisinde değerleri değiştirilebilir.
+//Sabit ya da değişken atamada tip açıkçada belirtilebilir var t : Int veya ilk değeri de verilebilir var t : Int = 27 gibi...
+
+print("a = \(a)")
+print("b = \(b)")
+print("c = \(c)")
+print("Sabitlerin Toplamı: \(a + b + c )")
+
+print("x = \(x) y = \(y) z = \(z)")
+toplam = x + y + z
+print("Değişkenlerin Toplamı : \(toplam)")
+
